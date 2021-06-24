de76f109e17be56a816cadc1b3300163f00617c0
Ashley Ma <euyam458689881@gmail.com>
2021-06-24 13:24:04 -0700

    add appt 30mins 1hour

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 //components
 import Footer from "./components/Footer.jsx";
-import Navbar from "./components/Navbar/Navbar.js";
+import Navbar from "./components/Navbar/header.jsx";
 //containers
 //homepage
 import WhatWeDo from "./Homepage/WhatWeDo.js";
@@ -15,49 +15,49 @@ import Appointment from "./containers/Appointment/scheduleMeeting.js";
 import ServingFresh from "./containers/ServingFresh/ServingFresh";
 import MealsForMe from "./containers/MealsForMe/MealsForMe";
 import How_we_can_help from "./containers/HowWeCanHelp/How_we_can_help.js";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 import React, { Component } from "react";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App" style={{ height: "auto" }}>
-                <BrowserRouter>
-                    <div className="App">
-                        <MessengerCustomerChat
-                            pageId="110111434630375"
-                            appId="941694233292147"
-                        />
-                        <Navbar />
-                        <Switch>
-                            <Route exact path="/" component={Main} />
-                            <Route path="/WhatWeDo" component={WhatWeDo} />
-                            <Route path="/how_we_can_help" component={How_we_can_help} />
-                            <Route path="/contact" component={Chat1} />
-                            <Route path="/portfolio" component={Portfolio} />
-                            <Route path="/careers" component={Careers} />
-                            <Route path="/servingfresh" component={ServingFresh} />
-                            <Route path="/mealsforMe" component={MealsForMe} />
-                            <Route path="/appointment" component={Appointment} />
-                        </Switch>
-                        <Footer></Footer>
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App" style={{ height: "auto" }}>
+        <BrowserRouter>
+          <div className="App">
+            <MessengerCustomerChat
+              pageId="110111434630375"
+              appId="941694233292147"
+            />
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route path="/WhatWeDo" component={WhatWeDo} />
+              <Route path="/how_we_can_help" component={How_we_can_help} />
+              <Route path="/contact" component={Chat1} />
+              <Route path="/portfolio" component={Portfolio} />
+              <Route path="/careers" component={Careers} />
+              <Route path="/servingfresh" component={ServingFresh} />
+              <Route path="/mealsforMe" component={MealsForMe} />
+              <Route path="/appointment" component={Appointment} />
+            </Switch>
+            <Footer></Footer>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export function Main() {
-    return (
-        <main style={{ textAlign: "center" }}>
-            <WhatWeDo />
-            <ScheduleAMeeting />
-            <Portfolio />
-            <Chat1 />
-        </main>
-    );
+  return (
+    <main style={{ textAlign: "center" }}>
+      <WhatWeDo />
+      <ScheduleAMeeting />
+      <Portfolio />
+      <Chat1 />
+    </main>
+  );
 }
 
 export default App;
