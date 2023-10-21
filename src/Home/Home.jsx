c615d7978c8358fe9f8441d572c83eb22df45651
Harshit Bhoraskar <harshit.bhoraskar@gmail.com>
2023-10-21 15:12:02 -0400

    revamped home/landing page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import {Box, Button} from '@material-ui/core';
-import '../Home/Home.css'
-import { useHistory, useLocation } from "react-router-dom";
-import ScrollToTop from "../Contact/ScrollToTop";
-import { Link } from "react-router-dom";
 
-// vote-page
+import '../Home/Home.css';
+
+import { useHistory, useLocation } from "react-router-dom";
 import Vote from "../voteModal/vote"
 import Modal from '@mui/joy/Modal';
 import ModalClose from '@mui/joy/ModalClose';
@@ -14,49 +12,26 @@ import ModalOverflow from '@mui/joy/ModalOverflow';
 import ModalDialog from '@mui/joy/ModalDialog';
 
 
-//import Backdrop from "../Assets/Backdrop.png"
+const Home =()=>{
 
-export default function Home(){
-
-   // for modal
-   const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
     const myRef = useRef(null)
-    // const myRef_project = useRef(null)
-    // const myRef_teams = useRef(null)
     const myRef_home = useRef(null)
-
 
     const history = useHistory();
     const location = useLocation();
 
 
-    // useEffect(()=>{
-    //     myRef_project.current.scrollIntoView({ behavior: 'smooth', block: 'start' })    
-    // },[location.state_project])
-
-    // useEffect(()=>{
-    // myRef_teams.current.scrollIntoView({ behavior: 'smooth', block: 'start' })    
-    // },[location.state_teams])
-
-    useEffect(()=>{
-        myRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })    
-    },[location.state])
-
-    useEffect(()=>{
-        window.scrollTo(0, 0); 
-    },[])
-
-    // vote-page code
-    React.useEffect(()=>{
-        setTimeout(()=> setOpen(true), 1000)
-  },[])
+//     React.useEffect(()=>{
+//         setTimeout(()=> setOpen(true), 1000)
+//   },[])
 
     return(
         <div  >
             {/* Vote-page code start*/}
 
-            <React.Fragment>
+    <React.Fragment>
       {/* <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
         Open modal
       </Button> */}
@@ -103,29 +78,109 @@ export default function Home(){
 
 
             {/* Vote-page Code end */}
+
             <Box ref={myRef_home} style={{width:'100%', backgroundImage:`url(${"./Navigation/Backdropv2.png"})`,backgroundRepeat:'no-repeat', backgroundSize:'100%, 200vh'}}>
             <Box className="HomeContainer">
                 <div  className="BoxContainer_1">
                     <div className="BoxContainer_1_Text_Header">
-                            Turning ideas into reality 
+                            Tiny Homes Competition
                     </div>
                     <div className="BoxContainer_1_Text_and_Image" >     
                         <div className="BoxContainer_1_Text">
                             <div  className="BoxContainer_1_Text_Body" >
-                                We love taking ideas from zero  to one whether they are  our own creations or yours as an entrepreneur
-                            </div> 
-                            <button className="BoxContainer_1_Text_Button"  onClick={()=> history.push("/contact")}>
-                                Schedule a free consultation
-                            </button>
-                        </div>
-                        <div className="BoxContainer_1_Image" >
-                            <img src={"./Navigation/Logo1.png"}  style={{width:'100%', objectFit:'contain'}}/>
-                        </div>     
-                    </div> 
-                </div>
 
-                <div  className="BoxContainer_2" >
-                    There are infinite ways to take an idea and turn into a business.
+                            We’e glad you are here. We’d love for your to take a look at our tiny home and We’d love for you to vote for our competition designs in the City of Tucson Casita Competition  
+                                
+                            </div> 
+                            
+                        </div>
+
+                        <div className="BoxContainer_1_Image" >
+                        <button className="BoxContainer_1_Text_Button"  onClick={()=> window.open("https://casitas-in-tucson-cotgis.hub.arcgis.com/pages/casita-competition-public-voting#ember271","_blank")}>
+                                Vote Now!
+                        </button>
+                        </div>
+
+                    </div> 
+                    <div>
+
+<table class="table-home">
+    <tr>
+        <td className="tucson-div">
+        <img alt="tucson" src="/artifacts/tucson.jpg" className="tucson-img-home" />
+        <h3>Help us make these designs better</h3>
+        </td>
+        <td>
+        <img className="image-dimesion-L-Casita-home" src='artifacts/lcasita.jpg' alt="L-Casita" />
+
+        </td>
+        <td>
+        <img className="image-dimesion-I-casita-home" src="artifacts/icasita.jpg" alt="I-Casita" />
+        </td>
+    </tr>
+    <tr>
+        <td className="">
+        {/* <div className="BoxContainer_1_Image" > */}
+            <button className="BoxContainer_1_Text_Button"  onClick={()=> window.open("https://casitas-in-tucson-cotgis.hub.arcgis.com/pages/casita-competition-public-voting#ember271","_blank")}>
+                    Feedback
+            </button>
+        {/* </div> */}
+       
+        </td>
+        <td>
+          L - Casita
+        </td>
+        <td>
+            I - Casita
+         </td>
+    </tr>
+</table>
+
+</div>     
+
+                </div>
+                <div className="BoxContainer_2">
+                <div  className="BoxContainer_1_Text_Header" >
+                    Turning Ideas into reality
+                </div>
+                <table class="box2">
+                    <tr>
+                        <td className="box2_text">
+                        We love taking ideas from zero to one whether they are our own creations or yours as an entrepreneur
+
+                        </td>
+                        <td className="box2_text2">
+                        <button className="BoxContainer_1_Text_Button"  onClick={()=> window.open("https://casitas-in-tucson-cotgis.hub.arcgis.com/pages/casita-competition-public-voting#ember271","_blank")}>
+                   See how we can help
+            </button>
+
+                        </td>
+                    </tr>
+                    </table>
+                    <table class="box2">
+                    <tr>
+                        <td className="cards">
+                            <div className="box3_text">
+                                <h2>About Us</h2>
+                                <button className="BoxContainer_1_Text_Button"  onClick={()=> window.open("https://casitas-in-tucson-cotgis.hub.arcgis.com/pages/casita-competition-public-voting#ember271","_blank")}>
+                   <span className="box-text-2">Learn More</span>
+            </button>
+                            </div>
+                        </td>
+                        <td className="cards">
+                            <div className="box3_text">
+                            <h2>Internships</h2>
+                                <button className="BoxContainer_1_Text_Button"  onClick={()=> window.open("https://casitas-in-tucson-cotgis.hub.arcgis.com/pages/casita-competition-public-voting#ember271","_blank")}>
+                                <span className="box-text-2">Learn More</span>
+            </button>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+                <div className="box2">
+                <div>
+                </div>
+                </div>
                 </div>
 
                 <div  className="BoxContainer_3" >
@@ -133,12 +188,16 @@ export default function Home(){
                         <div className="BoxContainer_3_Text_Header">
                         Projects we’ve built
                         </div>
-                        <div className="BoxContainer_3_Text_Body">
+                        {/* <div className="BoxContainer_3_Text_Body">
                         We are entrepreneurs solving problems that we encounter in new and creative ways. Here are some examples.
-                        </div>
+                        </div> */}
                     </div>
-                    <div className="BoxContainer_3_Text_Image" >
+                    <div className="in_box">
+                        <div className="BoxContainer_3_Text_Image" >
                         <div >
+                        {/* <div className="BoxContainer_3_Text_Header">
+                    Projects we’ve helped build
+                    </div> */}
                             <div >
                             <img src={"./Home/M4Me.png"} className="BoxContainer_3_Text_ImageSize_1" />
                             </div>
@@ -149,11 +208,14 @@ export default function Home(){
                         <div>
                             <img src={"./Home/Fun_Fact.png"}  className="BoxContainer_3_Text_ImageSize_2"/>
                         </div>
-                    </div>
-                    <div  style={{display:'flex', justifyContent:'center'}}>
+                        </div>
+                        <div  className="box_3_text">
+                        
+We are entrepreneurs solving problems that we encounter in new and creative ways. Here are some examples.
                         <button  className="BoxContainer_3_Text_Button" onClick={()=> history.push("/projects")}>
                                 View all Projects
                         </button>
+                        </div>
                     </div>
                 </div>
 
@@ -164,10 +226,9 @@ export default function Home(){
                     <div className="BoxContainer_3_Text_Header">
                     Projects we’ve helped build
                     </div>
-                    <div className="BoxContainer_3_Text_Body">
-                    We work with companies, non-profits, Universities and entrepreneurs to help them innovate and solve problems. Here are a few examples.            </div>
                 </div>
-                <div className="BoxContainer_3_Text_Image" >
+                <div className="in_box">
+                <div className="BoxContainer_4_Text_Image" >
                     <div>
                         <div style={{backgroundColor:'white'}}>
                         <img src={"./Home/NityaAyurvedaLogo.png"} className="BoxContainer_3_Text_ImageSize_1"/>
@@ -180,11 +241,23 @@ export default function Home(){
                         <img src={"./Home/Manifest.png"} className="BoxContainer_3_Text_ImageSize_2"/>
                     </div>
                 </div>
-                <div style={{display:'flex', justifyContent:'center'}}>
+                <div>
+                <div className="box_4_text  margin-top20">
+                    We work with companies, non-profits, Universities and entrepreneurs to help them innovate and solve problems. Here are a few examples.<br/>
+                    <button  className="BoxContainer_3_Text_Button" onClick={()=> history.push("/projects")}>
+                                View all Projects
+                        </button>
+            
+                    </div>
+
+
+                </div>
+                </div>
+                {/* <div style={{display:'flex', justifyContent:'center'}}>
                     <button className="BoxContainer_4_Text_Button"  onClick={()=> history.push("/contact")}>
                     Schedule a free consultation
                     </button>
-                </div>
+                </div> */}
             </div>
 
             <div className="BoxContainer_5">
@@ -207,7 +280,7 @@ export default function Home(){
                 </div>
             </div>
 
-            <div  className="BoxContainer_6" style={{marginTop:'-1rem'}} >
+            {/* <div  className="BoxContainer_6" style={{marginTop:'-1rem'}} >
                 <div  className="BoxContainer_6_Text_Header" >
                     Teams
                 </div>
@@ -228,10 +301,10 @@ export default function Home(){
                     Join the team
                     </button>
                 </div>
-            </div>
+            </div> */}
         
            
-            <div  className="BoxContainer_5">
+            {/* <div  className="BoxContainer_5">
                 <div className="BoxContainer_5_Text">
                     <div  className="BoxContainer_5_Text_Header" >
                     About Us
@@ -253,9 +326,13 @@ export default function Home(){
                     </button>
                 </div>
                 </div>
-            </div>
+            </div> */}
             </Box>
             </Box>
+
+
         </div>
-    )
+            )
 }
+
+export default Home;
